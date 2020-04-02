c592e5376867c26235e17c1c12d21fd93f900780
PS <example@gmail.com>
2020-04-02 17:46:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// alert("Hi");
+var leftBoarderWidht = 1;
+
+console.log(leftBoarderWidht);
+console.log(4/2);
+// let answer = confirm("Are you here?");
+let answer = +prompt("Are you here?", "Yes");
+
+console.log(answer);
